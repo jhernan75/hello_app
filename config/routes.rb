7316aea6a707411da00961601eba3dacf70148ba
jhernan75 <ubuntu@ip-172-31-19-146.us-east-2.compute.internal>
2020-05-20 15:38:38 +0000

    moreupdates

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   get 'static_pages/living'
   get 'static_pages/fun'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'static_pages#fun'
+  root 'static_pages#home'
 end
